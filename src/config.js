32b5d6674eafd6bc95f16117c830fbe2a4f0a2d5
Shaunna Beck <ShaunnaBeck@Shaunnas-MacBook-Pro.local>
2020-03-12 15:52:45 -0400

    ,,

--- src/config.js
+++ src/config.js
@@ -1,5 +1,5 @@
 module.exports = {
   PORT: process.env.PORT || 8080,
   NODE_ENV: process.env.NODE_ENV || 'development',
-  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'petful-78132dlym.now.sh',
+  CLIENT_ORIGIN: process.env.CLIENT_ORIGIN || 'https://petful-seven.now.sh',
 };
